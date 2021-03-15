9e3a2bc59414dd5e8d4e67b2be7186faf1768626
Donovan McGuire <donovanjmcguire@gmail.com>
2021-03-15 16:27:02 -0500

    Create Animals.js

---
+++ forms/animals/Animals.js
@@ -0,0 +1,10 @@
+
+let myAnimals = ['dog', 'cat', 'horse', 'meerkat']
+
+let userAnimal = prompt('Enter your favorite animal:')
+
+myAnimals.push(userAnimal.toLowerCase())
+
+let lastAnimal = myAnimals.slice(-1)[0]
+
+console.log(`The last animal is a/ an ${lastAnimal}.`) 
